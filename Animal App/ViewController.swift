8af7051d1ade952ed3e4d1e718e677289867b75f
jerrypm <jerrypurnamamaulid@gmail.com>
2024-01-10 14:41:58 +0700

    Initial Commit

---
+++ Animal App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animal App
+//
+//  Created by Jeri Purnama Maulid on 10/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
